623c524425486662f43eb00200ffede8b987cb4f
Corrie <corrie.e.grimshaw@gmail.com>
2022-08-19 07:42:18 -0700

    Add state to SingleRoomAvailability

--- src/Components/SingleRoomAvailability.jsx
+++ src/Components/SingleRoomAvailability.jsx
@@ -3,6 +3,12 @@ import React, {Component} from "react";
 const totalAvailableHoursArray = ["7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM", "8:00PM", "9:00PM"]
 
 class SingleRoomAvailability extends Component{
+	constructor(props) {
+    	super(props);
+    	this.state = {
+    		selectedHour: ""
+    	};
+	  }
 
 
 	renderHours(){
@@ -14,11 +20,13 @@ class SingleRoomAvailability extends Component{
 						hourClassName = "unavailable"
 					} else if (this.props.room.reservedHours.includes(e)) {
 						hourClassName = "reserved"
+					} else if (this.state.selectedHour === e) {
+						hourClassName = "selected"
 					} else {
 						hourClassName = e.slice(-2) === "AM" ? "AM" : "PM"
 					}
 					return (<div>
-						<div key={index} className={hourClassName}>{e}</div>
+						<div key={index} onClick={(e)=> this.hourClickHandler(e)} className={hourClassName} name={e}>{e}</div>
 						</div>
 						)
 				})}
@@ -26,6 +34,9 @@ class SingleRoomAvailability extends Component{
 			);
 	}
 
+	hourClickHandler(e){
+		this.setState({selectedHour: e.target.name })
+	}
 
 	render(){
 		return (
